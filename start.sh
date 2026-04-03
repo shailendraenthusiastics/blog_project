0504e0d1004fd63e46c0e5bae49f37cbc74826ba
Shailendra Kumar <sky04062005@gmail.com>
2026-04-03 18:37:26 +0530

    run migrations automatically on Render startup

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -o errexit
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+exec gunicorn blog.wsgi:application --bind 0.0.0.0:${PORT:-10000}
