4c6f8b01a4b39cb36ae83fbb36d857b2aa4b1f5b
Shailendra Kumar <sky04062005@gmail.com>
2026-04-03 23:39:15 +0530

    changes in code

--- start.sh
+++ start.sh
@@ -3,8 +3,6 @@ set -o errexit
 
 echo "Starting Django deploy bootstrap"
 python manage.py migrate --noinput
-python manage.py seed_blogs_if_empty
-python manage.py shell -c "from api.models import Blog; print('BLOG_COUNT_AT_STARTUP', Blog.objects.filter(is_active=True).count())"
 python manage.py collectstatic --noinput
 
 exec gunicorn blog.wsgi:application --bind 0.0.0.0:${PORT:-10000}
