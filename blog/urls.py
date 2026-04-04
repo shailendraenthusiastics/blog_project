c470eeea497866ac85283185e43e2541bd8a4277
Shailendra Kumar <sky04062005@gmail.com>
2026-04-04 22:55:52 +0530

    changes in the code

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -6,15 +5,18 @@ from django.conf.urls.static import static
 from api.views import BlogFrontendViewSet, BlogDetailPageViewSet, ckeditor_upload_view
 
 urlpatterns = [
-     path('admin/', admin.site.urls),
-    path('', BlogFrontendViewSet.as_view({'get': 'list'}), name='home'),
-    path('list/', BlogFrontendViewSet.as_view({'get': 'list'}), name='blog-frontend'),
-    path('blog-detail/', BlogDetailPageViewSet.as_view({'get': 'list'}), name='blog-detail-query'),
-    path('ckeditor/upload/', ckeditor_upload_view, name='ckeditor-upload'),
-    path('dashboard/', include('Admin.urls')), 
-    path('api/', include('api.urls')),
+    path("admin/", admin.site.urls),
+    path("", BlogFrontendViewSet.as_view({"get": "list"}), name="home"),
+    path("list/", BlogFrontendViewSet.as_view({"get": "list"}), name="blog-frontend"),
+    path(
+        "blog-detail/",
+        BlogDetailPageViewSet.as_view({"get": "list"}),
+        name="blog-detail-query",
+    ),
+    path("ckeditor/upload/", ckeditor_upload_view, name="ckeditor-upload"),
+    path("dashboard/", include("Admin.urls")),
+    path("api/", include("api.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
