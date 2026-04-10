3b05c7883ca4ba16e5479e99acae8b772668fd26
Shailendra Kumar <sky04062005@gmail.com>
2026-04-10 21:35:43 +0530

    changes in code

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -8,21 +8,8 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-from django.conf import settings
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
-
-if os.environ.get("RENDER", "").lower() == "true":
-    import django
-    from django.core.management import call_command
-
-    django.setup()
-    print(f"WSGI active MEDIA_ROOT: {settings.MEDIA_ROOT}")
-    # Defensive migration hook for platforms where start command may skip migrate.
-    call_command("migrate", interactive=False, verbosity=0)
-    # Defensive seed hook for platforms where start command may skip seed.
-    call_command("seed_render_content", verbosity=0)
 
 application = get_wsgi_application()
